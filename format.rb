edcc3c57d75761d78eec4fe4dbf9c470e8f8d523
poblouin <pblouin@wealthsimple.com>
2022-10-09 23:49:28 -0400

    rubocop

--- format.rb
+++ format.rb
@@ -4,7 +4,8 @@ require 'csv'
 
 input, output = ARGV
 
-INPUT_HEADERS = ["Folder Name", "Quantity", "Trade Quantity", "Card Name", "Set Code", "Set Name", "Card Number", "Condition", "Printing", "Language", "Price Bought", "Date Bought", "LOW", "MID", "MARKET"].freeze
+INPUT_HEADERS = ['Folder Name', 'Quantity', 'Trade Quantity', 'Card Name', 'Set Code', 'Set Name', 'Card Number',
+                 'Condition', 'Printing', 'Language', 'Price Bought', 'Date Bought', 'LOW', 'MID', 'MARKET'].freeze
 OUTPUT_HEADERS = ['Count', 'Tradelist Count', 'Name', 'Edition', 'Card Number', 'Condition', 'Language', 'Foil'].freeze
 
 input_csv = CSV.table(input, headers: INPUT_HEADERS)[2..]
@@ -22,4 +23,4 @@ output_csv = input_csv.each_with_object([OUTPUT_HEADERS]) do |row, memo|
   memo << [count, tradelist_count, name, edition, card_number, condition, language, foil]
 end
 
-File.write(output, output_csv.map { |c| c.join(",") }.join("\n"))
+File.write(output, output_csv.map { |c| c.join(',') }.join("\n"))
